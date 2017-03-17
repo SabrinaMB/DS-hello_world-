f2e2f3b53f5ac3b2c50b5073a58b494a20debd14
R09090909 <moreno_rebeca2010@hotmail.com>
2017-03-17 11:48:34 -0300

    Update exemplo1lernumero.py

--- exemplo1lernumero.py
+++ exemplo1lernumero.py
@@ -1,13 +1,11 @@
 
-def ler_numero():
-
-    n = int(input('Quantos numero tem na lista?'))
+def ler_numero(n):
     ler_numero = []
     while len(ler_numero) <= n: 
         k = int(input('Digite um numero'))
         ler_numero.append(k)
 
-   return (ler_numero)
+    return ler_numero
 
 
 
