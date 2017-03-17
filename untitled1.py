abf9c4ef15869d412097cf2a1d539de38c3e42a9
SabrinaMB <sbrnmchd@gmail.com>
2017-03-17 10:48:51 -0300

    Maior
    Encontra o numero maior

---
+++ untitled1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 17 10:44:20 2017
+
+@author: Sabrina
+"""
+
+
+def Numero_Maior(x):
+	p = 1
+	for i in range(ler_numero):
+		if x[i]>=p:
+			p=x[i]
+	return p
